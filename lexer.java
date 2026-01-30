f2dda1efe15fd04f4978188c1008da680abfe9e9
neon.light <a8985289889@gmail.com>
2026-01-30 22:18:45 +0600

    Add lexer class with token enumeration
    creting lexer

---
+++ lexer.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.the_language_programing_algraf;
+
+/**
+ *
+ * @author Acer
+ */
+public class The_language_programing_ALGRAF {
+    enum token{
+    IDENTIFIER,
+    KEYWORD
+    };
+    public static void main(String[] args) {
+       
+    }
+}
